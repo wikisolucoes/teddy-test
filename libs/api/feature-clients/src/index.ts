b269d8910006e3c67b49f8d041026d9540c04290
Felipe Alves <felipe.ci@hotmail.com>
2025-12-12 23:46:20 -0300

    feat: restructure FeatureClientsModule and index exports for improved organization

--- libs/api/feature-clients/src/index.ts
+++ libs/api/feature-clients/src/index.ts
@@ -1,1 +1,30 @@
-export * from './lib/feature-clients.module.js';
+// Module
+export { FeatureClientsModule } from './lib/feature-clients.module.js';
+
+// Domain
+export { Client } from './lib/domain/entities/client.entity.js';
+export { CPF } from './lib/domain/value-objects/cpf.vo.js';
+export { Phone } from './lib/domain/value-objects/phone.vo.js';
+
+// Application - Ports
+export { ClientRepository } from './lib/application/ports/client.repository.js';
+
+// Application - DTOs
+export {
+  type ClientDto,
+} from './lib/application/shared/dtos/client.dto.js';
+export {
+  CreateClientSchema,
+  type CreateClientDto,
+} from './lib/application/create-client/dtos/create-client.dto.js';
+export {
+  ListClientsSchema,
+  type ListClientsDto,
+} from './lib/application/list-clients/dtos/list-clients.dto.js';
+export {
+  UpdateClientSchema,
+  type UpdateClientDto,
+} from './lib/application/update-client/dtos/update-client.dto.js';
+
+// Application - Service
+export { ClientService } from './lib/application/services/client.service.js';
--- libs/api/feature-clients/src/lib/feature-clients.module.ts
+++ libs/api/feature-clients/src/lib/feature-clients.module.ts
@@ -1,8 +1,44 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClientsController } from './presentation/controllers/clients.controller.js';
+import { ClientService } from './application/services/client.service.js';
+import { ClientRepository } from './application/ports/client.repository.js';
+import { TypeOrmClientRepository } from './infrastructure/persistence/typeorm/repositories/client.repository.js';
+import { ClientSchema } from './infrastructure/persistence/typeorm/schemas/client.schema.js';
+import { CreateClientHandler } from './application/create-client/usecases/create-client.handler.js';
+import { UpdateClientHandler } from './application/update-client/usecases/update-client.handler.js';
+import { DeleteClientHandler } from './application/delete-client/usecases/delete-client.handler.js';
+import { ListClientsHandler } from './application/list-clients/usecases/list-clients.handler.js';
+import { GetClientHandler } from './application/get-client/usecases/get-client.handler.js';
+
+const CommandHandlers = [
+  CreateClientHandler,
+  UpdateClientHandler,
+  DeleteClientHandler,
+];
+
+const QueryHandlers = [
+  ListClientsHandler,
+  GetClientHandler,
+];
 
 @Module({
-  controllers: [],
-  providers: [],
-  exports: [],
+  imports: [
+    CqrsModule,
+    TypeOrmModule.forFeature([ClientSchema], 'read'),
+    TypeOrmModule.forFeature([ClientSchema], 'write'),
+  ],
+  controllers: [ClientsController],
+  providers: [
+    ClientService,
+    {
+      provide: ClientRepository,
+      useClass: TypeOrmClientRepository,
+    },
+    ...CommandHandlers,
+    ...QueryHandlers,
+  ],
+  exports: [ClientRepository, ClientService],
 })
 export class FeatureClientsModule {}
